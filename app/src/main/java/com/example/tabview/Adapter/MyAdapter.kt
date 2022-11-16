b64c05d9b07263f8e2c4d94680a1d9312789199e
Megy Permana <smamegy@gmail.com>
2022-11-17 00:36:24 +0700

    TabLayout

---
+++ app/src/main/java/com/example/tabview/Adapter/MyAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.tabview.Adapter
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.tabview.Fragments.Home
+import com.example.tabview.Fragments.Profile
+import com.example.tabview.Fragments.Settings
+
+internal class MyAdapter(var context: Context, fm: FragmentManager, var totalTabs: Int): FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> {
+                Home()
+            }
+            1 -> {
+                Profile()
+            }
+            2 -> {
+                Settings()
+            }
+            else -> getItem(position)
+        }
+    }
+    override fun getCount(): Int {
+        return totalTabs
+    }
+
+}
